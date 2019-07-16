600c439503e2a0a1a745ec7a9cf242d47a8dcffe
boppleton <bots@boppleton.com>
2019-07-16 15:35:39 -0400

    -

--- db.js
+++ db.js
@@ -58,7 +58,7 @@ module.exports = {
         client.query('UPDATE '+name+'\n' +
             vars +
             // 'set outtime = now()\n' +
-            ' WHERE id = (select max(id) from '+name+');', (err, res) => {
+            ' WHERE id = (select max(ID) from '+name+');', (err, res) => {
             if (err) throw err
             for (let row of res.rows) {
                 console.log(JSON.stringify(row))
