af1f379bfa5e406c03f9dcc6c3f1911c9eb56ed3
boppleton <bots@boppleton.com>
2019-07-16 11:34:12 -0400

    create trades, strat, add api server

---
+++ api.js
@@ -0,0 +1,27 @@
+///
+/// setup the api server with base endpoint, returns obj to set GETs and POSTS from
+///
+
+const express = require('express')
+
+module.exports = {
+
+    start: () => {
+
+        const api = express()
+        const port = process.env.PORT || 3001
+
+        //root endpoint
+        api.get('/', (req, res) => res.send('[///]'))
+
+        // Parse URL-encoded bodies (as sent by HTML forms)
+        api.use(express.urlencoded());
+
+        // Parse JSON bodies (as sent by API clients)
+        api.use(express.json());
+
+        api.listen(port, () => console.log(`api server started on port ${port}`))
+
+        return api
+    }
+}
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 console.log('nodetest2 startt')
 
+
+
+const api = require('./api.js').start()
+
+
+
+
 const { Client } = require('pg');
 
 const client = new Client({
@@ -21,6 +28,56 @@ client.connect();
 //     client.end();
 // });
 
+client.query('CREATE TABLE trades (\n' +
+    '  ID SERIAL PRIMARY KEY,\n' +
+    '  active BOOLEAN,\n' +
+    '  trigger VARCHAR(30),\n' +
+    '  entryPrice NUMERIC,\n' +
+    '  entrySize NUMERIC,\n' +
+    '  tpPrice NUMERIC,\n' +
+    '  stopPrice NUMERIC,\n' +
+
+    '  startBalance NUMERIC,\n' +
+    '  endingBalance NUMERIC,\n' +
+    '  pnl NUMERIC,\n' +
+    '  diff VARCHAR(50),\n' +
+    '  resultType VARCHAR(30),\n' +
+    '  resultMove NUMERIC,\n' +
+    '  filled NUMERIC,\n' +
+    '  startTime NUMERIC,\n' +
+    '  endTime NUMERIC,\n' +
+    '  endPrice NUMERIC,\n' +
+    '  startStartEquity NUMERIC\n' +
+
+
+    ');', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+
+client.query('CREATE TABLE strat (\n' +
+    '  ID SERIAL PRIMARY KEY,\n' +
+    '  name VARCHAR(30),\n' +
+    '  symbol VARCHAR(30),\n' +
+    '  tf NUMERIC\n,' +
+    '  tpPercent NUMERIC\n,' +
+    '  stopPercent NUMERIC\n,' +
+    '  size NUMERIC\n,' +
+    '  scalePercent NUMERIC\n,' +
+    '  scaleQty NUMERIC\n,' +
+    '  scaleWeight NUMERIC\n,' +
+    '  trigger VARCHAR(30)\n,' +
+    '  scaleChase BOOLEAN\n,' +
+    ');', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
 
 // client.query('INSERT INTO log (text, time)\n' +
 //     '  VALUES (\'Jerry\', 11), (\'George\', 22);', (err, res) => {
