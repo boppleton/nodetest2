6e918c5ff6e1a5b936f612e283d2fde78910bf3f
boppleton <bots@boppleton.com>
2019-07-16 11:11:45 -0400

    create table log

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('nodetest2 start')
+console.log('nodetest2 startt')
+
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
+
+client.query('CREATE TABLE log (\n' +
+    '  ID SERIAL PRIMARY KEY,\n' +
+    '  text VARCHAR(200),\n' +
+    '  time BIGINT\n' +
+    ');', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+
