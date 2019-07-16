bfa9c63d960007a9bbbbe131bbe1c849cdcac474
boppleton <bots@boppleton.com>
2019-07-16 11:14:30 -0400

    -

--- index.js
+++ index.js
@@ -9,11 +9,28 @@ const client = new Client({
 
 client.connect();
 
-client.query('CREATE TABLE log (\n' +
-    '  ID SERIAL PRIMARY KEY,\n' +
-    '  text VARCHAR(200),\n' +
-    '  time BIGINT\n' +
-    ');', (err, res) => {
+// client.query('CREATE TABLE log (\n' +
+//     '  ID SERIAL PRIMARY KEY,\n' +
+//     '  text VARCHAR(200),\n' +
+//     '  time BIGINT\n' +
+//     ');', (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//         console.log(JSON.stringify(row));
+//     }
+//     client.end();
+// });
+
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT * FROM log', (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
         console.log(JSON.stringify(row));
