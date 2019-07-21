0cc6642ff4529c9517e172fa9a62ffb6afdea535
boppleton <bots@boppleton.com>
2019-07-20 22:12:47 -0400

    -

--- index.js
+++ index.js
@@ -445,7 +445,6 @@ if (!msgg.result) {
 
         //fix accuracy after
         if (t.active) {
-
             if (nodelet.currentEntry > 0) {
                 t.entryPrice = nodelet.currentEntry
             }
@@ -455,6 +454,13 @@ if (!msgg.result) {
 
         t.diff = (((t.endBalance - t.startBalance) / t.startBalance) * 100).toFixed(2) + "%  ($" + ((t.endBalance - t.startBalance) * nodelet.currentBid).toFixed(2) + ")"
 
+        db.update('trades'+nodelet.id, 'pnl='+t.pnl)
+
+        db.get('trades'+nodelet.id, (trades)=> {
+
+            console.log('tradesdb: ' + JSON.stringify(trades))
+
+        })
 
         // db.update('trades'+nodelet.id,
         //     'endBalance=' + nodelet.trades[0].endBalance + ', ' +
