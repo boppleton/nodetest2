434363117f10331383e696952e1c43a9d4bd78ad
boppleton <bots@boppleton.com>
2019-07-21 20:20:47 -0400

    -

--- index.js
+++ index.js
@@ -22,12 +22,9 @@ db.start()
 //
 // return
 
-
-
-
-db.truncate('log1')
+// db.truncate('log1')
 // db.truncate('log2')
-db.truncate('trades1')
+// db.truncate('trades1')
 // db.truncate('trades2')
 // db.truncate('strat1')
 // db.truncate('strat2')
